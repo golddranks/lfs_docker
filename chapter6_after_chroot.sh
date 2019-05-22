2c0668e6c2afe03ae6c9a4c4e46d2b0c15c93f2e
Pyry Kontio <kontio@r.recruit.co.jp>
2019-05-22 18:38:13 +0900

    Split chapter 6 to before/after chroot

--- chapter6_after_chroot.sh
+++ chapter6_after_chroot.sh
@@ -5,32 +5,9 @@ alias trace_on='set -x'
 alias trace_off='{ set +x; } 2>/dev/null'
 
 echo ""
-echo "Setting the LFS environment up and chrooting."
+echo "Setting up the environment (creating directories and settings files)"
 
 set -x
-
-mkdir -pv $LFS/{dev,proc,sys,run}
-
-mknod -m 600 $LFS/dev/console c 5 1
-mknod -m 666 $LFS/dev/null c 1 3
-
-mount -v --bind /dev $LFS/dev
-
-mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
-mount -vt proc proc $LFS/proc
-mount -vt sysfs sysfs $LFS/sys
-mount -vt tmpfs tmpfs $LFS/run
-
-if [ -h $LFS/dev/shm ]; then
-  mkdir -pv $LFS/$(readlink $LFS/dev/shm)
-fi
-
-chroot "$LFS" /tools/bin/env -i \
-    HOME=/root                  \
-    TERM="$TERM"                \
-    PS1='(lfs chroot) \u:\w\$ ' \
-    PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
-    /tools/bin/bash --login +h
 
 mkdir -pv /{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
 mkdir -pv /{media/{floppy,cdrom},sbin,srv,var}
---
+++ chapter6_before_chroot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+shopt -s expand_aliases
+alias trace_on='set -x'
+alias trace_off='{ set +x; } 2>/dev/null'
+
+echo ""
+echo "Setting the LFS environment up and chrooting."
+
+set -x
+
+mkdir -pv $LFS/{dev,proc,sys,run}
+
+mknod -m 600 $LFS/dev/console c 5 1
+mknod -m 666 $LFS/dev/null c 1 3
+
+mount -v --bind /dev $LFS/dev
+
+mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+mount -vt tmpfs tmpfs $LFS/run
+
+if [ -h $LFS/dev/shm ]; then
+  mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+fi
+
+cp chapter6_chroot.sh $LFS
+
+chroot "$LFS" /tools/bin/env -i \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
+    /tools/bin/bash --login +h
